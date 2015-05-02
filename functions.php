1497c1c3873769f8d2c88df97ffc78969644fc13
Nick Gaswirth <nick@roundhouse-designs.com>
2015-05-01 21:39:58 -0400

    tweaks

--- functions.php
+++ functions.php
@@ -64,23 +64,23 @@ function rhd_override_scripts() {
  **/
 function rhd_favicons() {
 	echo '
-		<link rel="shortcut icon" href="' . RHD_THEME_DIR . '/favicon/favicon.ico">
-		<link rel="apple-touch-icon" sizes="57x57" href="' . RHD_THEME_DIR . '/favicon/apple-touch-icon-57x57.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="' . RHD_THEME_DIR . '/favicon/apple-touch-icon-114x114.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="' . RHD_THEME_DIR . '/favicon/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="144x144" href="' . RHD_THEME_DIR . '/favicon/apple-touch-icon-144x144.png">
-		<link rel="apple-touch-icon" sizes="60x60" href="' . RHD_THEME_DIR . '/favicon/apple-touch-icon-60x60.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="' . RHD_THEME_DIR . '/favicon/apple-touch-icon-120x120.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="' . RHD_THEME_DIR . '/favicon/apple-touch-icon-76x76.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="' . RHD_THEME_DIR . '/favicon/apple-touch-icon-152x152.png">
-		<link rel="icon" type="image/png" href="' . RHD_THEME_DIR . '/favicon/favicon-196x196.png" sizes="196x196">
-		<link rel="icon" type="image/png" href="' . RHD_THEME_DIR . '/favicon/favicon-160x160.png" sizes="160x160">
-		<link rel="icon" type="image/png" href="' . RHD_THEME_DIR . '/favicon/favicon-96x96.png" sizes="96x96">
-		<link rel="icon" type="image/png" href="' . RHD_THEME_DIR . '/favicon/favicon-16x16.png" sizes="16x16">
-		<link rel="icon" type="image/png" href="' . RHD_THEME_DIR . '/favicon/favicon-32x32.png" sizes="32x32">
+		<link rel="shortcut icon" href="' . RHD_CHILD_DIR . '/favicon/favicon.ico">
+		<link rel="apple-touch-icon" sizes="57x57" href="' . RHD_CHILD_DIR . '/favicon/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="' . RHD_CHILD_DIR . '/favicon/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="' . RHD_CHILD_DIR . '/favicon/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="' . RHD_CHILD_DIR . '/favicon/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="' . RHD_CHILD_DIR . '/favicon/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="' . RHD_CHILD_DIR . '/favicon/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="' . RHD_CHILD_DIR . '/favicon/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="' . RHD_CHILD_DIR . '/favicon/apple-touch-icon-152x152.png">
+		<link rel="icon" type="image/png" href="' . RHD_CHILD_DIR . '/favicon/favicon-196x196.png" sizes="196x196">
+		<link rel="icon" type="image/png" href="' . RHD_CHILD_DIR . '/favicon/favicon-160x160.png" sizes="160x160">
+		<link rel="icon" type="image/png" href="' . RHD_CHILD_DIR . '/favicon/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="' . RHD_CHILD_DIR . '/favicon/favicon-16x16.png" sizes="16x16">
+		<link rel="icon" type="image/png" href="' . RHD_CHILD_DIR . '/favicon/favicon-32x32.png" sizes="32x32">
 		<meta name="msapplication-TileColor" content="#da532c">
-		<meta name="msapplication-TileImage" content="' . RHD_THEME_DIR . '/favicon/mstile-144x144.png">
-		<meta name="msapplication-config" content="' . RHD_THEME_DIR . '/favicon/browserconfig.xml">
+		<meta name="msapplication-TileImage" content="' . RHD_CHILD_DIR . '/favicon/mstile-144x144.png">
+		<meta name="msapplication-config" content="' . RHD_CHILD_DIR . '/favicon/browserconfig.xml">
 	';
 }
 add_action( 'wp_head', 'rhd_favicons' );
