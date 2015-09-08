69175798e9553c876b6dfde6987b48a6fbe5eef5
Nick Gaswirth <nick@roundhouse-designs.com>
2015-09-08 18:23:42 -0400

    Pre-launch

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage rhd
+ */
+?>
+
+	</main><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+		<?php get_sidebar( 'footer' ); ?>
+		<div class="site-info">
+			<p>
+				<?php echo '&copy;' . date( 'Y' ); ?> <a href="mailto:nicoledalto@gmail.com"><?php echo bloginfo( 'name' ); ?></a> <?php echo ( rhd_is_mobile() ) ? '<br>' : '|&nbsp;'; ?>Site by <a href="//roundhouse-designs.com" target="_blank">Roundhouse<img id="rhd-logo-footer" src="//assets.roundhouse-designs.com/images/rhd-white-house.png" alt="Roundhouse Designs">Designs
+			</p>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
